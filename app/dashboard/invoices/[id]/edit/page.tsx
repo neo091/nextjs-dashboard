e5b2fd2faf1604e9729e1cd53c5173602b071035
neo091 <meo666@gmail.com>
2026-01-31 17:41:29 +0100

    Refactor code structure for improved readability and maintainability

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,37 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data"
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs"
+import Form from "@/app/ui/invoices/edit-form"
+import { notFound } from "next/navigation"
+import { use } from "react"
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ])
+
+  if (!invoice) {
+    notFound()
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  )
+}
